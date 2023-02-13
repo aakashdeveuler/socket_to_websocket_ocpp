0272c1264995a48d511e8eab0cbc53963a0891f6
Aakash Dev <aakash.dev@eulermotors.com>
2023-02-13 12:31:09 +0530

    new

--- socket_to_websocket.py
+++ socket_to_websocket.py
@@ -1,11 +1,17 @@
 import asyncio
+import socket
 import websockets
 
 async def handle_socket_data(websocket, path):
-    # Wait for data from the socket
-    data = await websocket.recv()
-    # Send the data to the socket
+    # Connect to the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 12345))
+    # Receive data from the socket
+    data = sock.recv(1024).decode()
+    # Send the data to the WebSocket
     await websocket.send(data)
+    # Close the socket connection
+    sock.close()
 
 # Start the WebSocket server
 start_server = websockets.serve(handle_socket_data, 'localhost', 8765)
