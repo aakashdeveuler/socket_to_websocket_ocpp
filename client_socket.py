7bdc8ec4c27585251a3edf35fbd78775eb9dd1de
Aakash Dev <aakash.dev@eulermotors.com>
2023-02-16 17:12:30 +0530

    threading added

--- client_socket.py
+++ client_socket.py
@@ -5,11 +5,11 @@ clientSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 clientSocket.connect(("fe80::c166:bbb6:ecf4:24c5%14",12345))
 print("connects")
 
-data = '[2, "97496a9r-867k-4d28-969d-10f795ff4545", "BootNotification", {"chargePointVendor": "", "chargePointModel": "Euler", "chargePointSerialNumber": "", "chargeBoxSerialNumber": "", "firmwareVersion": "", "iccid": "", "imsi": "", "meterSerialNumber": "", "meterType": ""}]'
+data = '[2, "97496a9r-867k-4d28-969d-10f795ff4545", "BootNotification", {"chargePointVendor": "123", "chargePointModel": "Euler", "chargePointSerialNumber": "", "chargeBoxSerialNumber": "", "firmwareVersion": "", "iccid": "", "imsi": "", "meterSerialNumber": "", "meterType": ""}]'
 clientSocket.send(data.encode())
 
 dataFromServer =  clientSocket.recv(1024)
 
 print("message from steve to tcu 1")
 print(dataFromServer.decode())
-clientSocket.close()
+# clientSocket.close()
--- thread_Charge_point_main.py
+++ thread_Charge_point_main.py
@@ -3,7 +3,7 @@ import logging
 import json
 import websockets
 import socket
-# import mysql.connector
+import threading
 socket.getaddrinfo('fe80::c166:bbb6:ecf4:24c5%14', 12345)
 
 from ocpp.v16 import ChargePoint as cp
@@ -12,15 +12,10 @@ from ocpp.v16.enums import RegistrationStatus
 
 logging.basicConfig(level=logging.INFO)
 
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="root",
-#   database="socketsteve"
-# )
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 class ChargePoint(cp):
-    async def send_boot_notification(self):
+    async def send_boot_notification(self,data):
         request = call.BootNotificationPayload(
             charge_point_model=data.get("chargePointModel"), charge_point_vendor=data.get("chargePointVendor")
         )
@@ -30,45 +25,50 @@ class ChargePoint(cp):
             print("Connected to central system.")
 
 
-async def main():
-    recei_data = ""
+async def main(data):
+    receive_data=""
     async with websockets.connect(
         "ws://43.205.177.121:8080/steve/websocket/CentralSystemService/1234", subprotocols=["ocpp1.6"]
     ) as ws:
 
         cp = ChargePoint("1234", ws)
         
-        val = await asyncio.gather(cp.start(), cp.send_boot_notification())
-        recei_data = val[0]
-    return recei_data
+        val = await asyncio.gather(cp.start(), cp.send_boot_notification(data))
+        receive_data = val[0]
+    return receive_data
 
 
 if __name__ == "__main__":
     # asyncio.run() is used when running this example with Python >= 3.7v
+    print("[STARTING] server is starting ....")
+    
     while(True):
+        def handle_client(conn, addr):
+            data = {}
+            print(f"[NEW CONNECTION] {addr} connected.")
+            connected = True
+            while connected:
+                print("Accepted a connection request from %s:%s"%(addr[0], addr[1]))
+                dataFromClient = conn.recv(1024).decode()
+                if dataFromClient == DISCONNECT_MESSAGE:
+                    connected = False
+                list = json.loads(dataFromClient)
+                data=list[3]
+                receive_data = asyncio.run(main(data))
+                clientConnected.send(receive_data.encode())
+                break
+            conn.close()
+        
         serverSocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         serverSocket.bind(("fe80::c166:bbb6:ecf4:24c5%14", 12345))
-        
-        print("socket connected")
         serverSocket.listen()
+                                
+            
         while(True):
             (clientConnected, clientAddress) = serverSocket.accept()
-            print("Accepted a connection request from %s:%s"%(clientAddress[0], clientAddress[1]))
-            dataFromClient = clientConnected.recv(1024)
-            Decoded_data=dataFromClient.decode()
-            list = json.loads(Decoded_data)
-            data = list[3]
-            print("checkkkkkkkkkkkkkkkkkkk")
-            print(type(data))
-            
-            # mycursor = mydb.cursor()
-
-            recei_data = asyncio.run(main())
-            # mycursor.execute("SELECT * FROM bootnotificationtotcu")
-            # rows = mycursor.fetchall()
-            # print("Data sending to client")
-            # # print(rows[-1])
-            # sendtcu = str(rows[-1][1])
-            clientConnected.send(recei_data.encode())
+            thread = threading.Thread(target=handle_client, args=(clientConnected, clientAddress))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
             break
+
         
